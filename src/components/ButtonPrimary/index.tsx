db727ee7807ad8929e4c58cfe9183b4da3f3c9f0
Sofia Fonseca <sofia.fonseca277@gmail.com>
2023-10-27 20:22:58 +0100

    Other components of details page

---
+++ src/components/ButtonPrimary/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function ButtonPrimary() {
+
+    return (
+        <div className="dsc-btn dsc-btn-blue">
+            Comprar
+        </div>
+    );
+}
+
+export default ButtonPrimary
---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function ProductCategory() {
+
+    return (
+        <div className="dsc-category">
+            Eletrônicos
+        </div>
+    );
+}
+
+export default ProductCategory
