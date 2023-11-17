4b7aa8da53585384e4cc194f008e29b21c185806
Sofia Fonseca <sofia.fonseca277@gmail.com>
2023-11-17 18:44:15 +0000

    Props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,29 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { ProductDTO } from '../../models/product';
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "Ecran 30 cm",
+  imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/9b5846b1157b296c3012f16c817f70ad1929fa6f/backend/img/2-big.jpg?raw=true",
+  price: 2500.99,
+  categories: [
+    {
+      id: 2,
+      name: "Electrônicos"
+    },
+    {
+      id: 3,
+      name: "Computadores"
+    },
+    {
+      id: 4,
+      name: "Importados"
+    }
+  ]
+}
 
 function ProductDetails() {
 
@@ -11,7 +34,7 @@ function ProductDetails() {
         <HeaderClient />
         <main>
           <section id="product-details-section" className="dsc-container">
-            <ProductDetailsCard />
+            <ProductDetailsCard product={product} />
             <div className="dsc-btn-page-container">
               <ButtonPrimary />
               <ButtonInverse />
