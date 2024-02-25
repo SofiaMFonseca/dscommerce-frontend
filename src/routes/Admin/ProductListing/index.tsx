d26ac1d8cd35e98d18bf35496404707a2bd34676
Sofia Fonseca <sofia.fonseca277@gmail.com>
2024-02-25 18:09:07 +0000

    Confirmation dialog modal

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -7,6 +7,7 @@ import { ProductDTO } from '../../../models/product';
 import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import DialogInfo from '../../../components/DialogInfo';
+import DialogConfirmation from '../../../components/DialogConfirmation';
 
 type QueryParams = {
     page: number;
@@ -18,6 +19,11 @@ function ProductListing() {
     const [dialogInfoData, setDialogInfoData] = useState({
         visible: false,
         message: "Operação com sucesso!"
+    });
+
+    const [dialogConfirmationData, setDialogConfirmationData] = useState({
+        visible: false,
+        message: "Tem certeza?"
     });
 
     const [isLastPage, setIsLastPage] = useState(false);
@@ -52,7 +58,11 @@ function ProductListing() {
     }
 
     function handleDeleteClick() {
-        setDialogInfoData({ ...dialogInfoData, visible: true });
+        setDialogConfirmationData({ ...dialogConfirmationData, visible: true });
+    }
+
+    function handleDialogConfirmationAnswer(answer: boolean) {
+        setDialogConfirmationData({ ...dialogConfirmationData, visible: false });
     }
 
     return (
@@ -104,6 +114,14 @@ function ProductListing() {
                 <DialogInfo
                     message={dialogInfoData.message}
                     onDialogClose={handleDialogInfoClose}
+                />
+            }
+
+            {
+                dialogConfirmationData.visible &&
+                <DialogConfirmation
+                    message={dialogConfirmationData.message}
+                    onDialogAnswer={handleDialogConfirmationAnswer}
                 />
             }
         </main>
