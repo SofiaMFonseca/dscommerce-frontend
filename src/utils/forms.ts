b6658761c145cb8ce3de74fd8ecad99878887126
Sofia Fonseca <sofia.fonseca277@gmail.com>
2024-02-28 19:01:58 +0000

    forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,4 +1,13 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function update(inputs: any, name: string, newValue: any) {
     return { ...inputs, [name]: { ...inputs[name], value: newValue} };
+}
+
+export function toValues(inputs: any) {
+    const data: any = {};
+    for (let name in inputs) {
+        data[name] = inputs[name].value;
+    }
+    return data;
 }
