c9a902af07d87475028dd24d6d3a702d4420a0e6
Sofia Fonseca <sofia.fonseca277@gmail.com>
2024-03-02 18:38:14 +0000

    Error CSS

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -50,7 +50,9 @@ function ProductForm() {
     }, []);
 
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setFormData(forms.update(formData, event.target.name, event.target.value));
+        const dataUpdated = forms.update(formData, event.target.name, event.target.value);
+        const dataValidated = forms.validate(dataUpdated, event.target.name);
+        setFormData(dataValidated);
     }
 
     return (
@@ -66,6 +68,7 @@ function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className='dsc-form-error'>{formData.name.message}</div>
                             </div>
                             <div>
                                 <FormInput
@@ -73,6 +76,7 @@ function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className='dsc-form-error'>{formData.price.message}</div>
                             </div>
                             <div>
                                 <FormInput
